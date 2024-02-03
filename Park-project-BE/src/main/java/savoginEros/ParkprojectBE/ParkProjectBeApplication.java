adce69cce725f2986226193ad4d8c075d58cc88c
Eros Savogin <eros.sav@gmail.com>
2024-02-03 19:40:41 +0100

    initial config

---
+++ Park-project-BE/src/main/java/savoginEros/ParkprojectBE/ParkProjectBeApplication.java
@@ -0,0 +1,13 @@
+package savoginEros.ParkprojectBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkProjectBeApplication.class, args);
+	}
+
+}
---
+++ Park-project-BE/src/test/java/savoginEros/ParkprojectBE/ParkProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package savoginEros.ParkprojectBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkProjectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
